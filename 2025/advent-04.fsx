5863fd35cf197d19e5aff3641853435a09b8baa1
Rob Kayman <rkayman@gmail.com>
2025-12-23 03:41:49 -0800

    Solves Advent of Code 2025 Day 4, Part 1
    Implements the solution for Advent of Code 2025, Day 4, Part 1.
    
    Parses the input grid of characters and identifies locations where adjacent rolls have less than 4 accessible rolls.

--- 2025/advent-04.fsx
+++ 2025/advent-04.fsx
@@ -33,11 +33,47 @@ module Example =
 
 module Problem =
     let parse (input: string) =
-        input.Trim().Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
-        |> Array.map (fun line -> line.ToCharArray())
+        input
+            .Trim()
+            .Split(Environment.NewLine, StringSplitOptions.TrimEntries ||| StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map _.ToCharArray()
+
+    let findAccessibleRolls (grid: char[][]) =
+        let rows = grid.Length
+        let cols = grid[0].Length
+
+        let isValid max value = value >= 0 && value < max
+        let isValidRow = isValid rows
+        let isValidCol = isValid cols
+        let notItself (r, c) (x, y) = not (r = x && c = y)
+        let adjacentRolls r c =
+            seq {
+                for i in r - 1 .. r + 1 do
+                    for j in c - 1 .. c + 1 do
+                        if (isValidRow i)
+                           && (isValidCol j)
+                           && (notItself (r, c) (i, j))
+                        then yield (i, j)
+            }
+
+        seq {
+            for r in 0 .. rows - 1 do
+                for c in 0 .. cols - 1 do
+                    if grid[r][c] = '@' then
+                        let accessibleRolls =
+                            adjacentRolls r c
+                            |> Seq.filter (fun (i, j) -> grid[i][j] = '@')
+                            |> Seq.length
+
+                        if accessibleRolls < 4 then
+                            yield (r, c), accessibleRolls
+        }
 
     let solve (input: string) =
-        let grid = parse input
+        parse input
+        |> findAccessibleRolls
+        |> Seq.length
 
-// File.ReadAllText "2025/input-04.txt"
+File.ReadAllText "2025/input-04.txt"
 // Example.given
+|> Problem.solve
