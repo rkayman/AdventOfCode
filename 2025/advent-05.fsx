db963e9464cabf4a74fda8e69fc987edcc86cc2f
Rob Kayman <rkayman@gmail.com>
2025-12-24 10:05:29 -0800

    Solves Advent of Code 2025 Day 5, Part 1
    Implements a solution to filter available IDs based on a set of fresh ranges.
    
    Parses the input to extract fresh ranges and available IDs, then filters the IDs to count those that fall within any of the fresh ranges.

---
+++ 2025/advent-05.fsx
@@ -0,0 +1,61 @@
+open System
+open System.IO
+
+module Example =
+    
+    let given = """
+        3-5
+        10-14
+        16-20
+        12-18
+
+        1
+        5
+        8
+        11
+        17
+        32
+        """
+    
+    let expected = 3
+    
+    
+module Problem =
+
+    type IngredientDB =
+        {
+            freshRanges: (int64 * int64) array
+            availableIds: int64 array
+        }
+
+    let parse (input: string) =
+        let doubleNewLine = Environment.NewLine + Environment.NewLine
+        let splitOptions = StringSplitOptions.RemoveEmptyEntries ||| StringSplitOptions.TrimEntries
+        let sections = input.Split(doubleNewLine, splitOptions)
+        let freshRanges =
+            sections[0]
+                .Split(Environment.NewLine, splitOptions)
+                |> Array.map (_.Split("-", splitOptions))
+                |> Array.map (fun range -> (int64 range[0], int64 range[1]))
+        let availableIds =
+            sections[1]
+                .Split(Environment.NewLine, splitOptions)
+                |> Array.map int64
+        { freshRanges = freshRanges; availableIds = availableIds }
+
+    let inline filter x (min, max) =
+        Int64.Clamp(x, min, max) = x
+    
+    let inline exists xs x =
+        xs |> Array.exists (filter x)
+    
+    let solve (input: string) : int =
+        let db = parse input
+        db.availableIds
+        |> Array.filter (exists db.freshRanges)
+        |> Array.length
+    
+    
+File.ReadAllText "2025/input-05.txt"
+// Example.given
+|> Problem.solve
