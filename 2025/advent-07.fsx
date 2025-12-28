6b373e70589e34885b88845341d1a4a704c11b41
Rob Kayman <rkayman@gmail.com>
2025-12-27 23:19:13 -0800

    Implements path traversal algorithm with memoization
    Implements an algorithm to traverse a grid based on specific rules.
    
    The algorithm starts at a defined position and explores the grid, keeping track of visited locations and special "splitter" locations. It accumulates the number of splits hit and the locations visited.
    
    The algorithm handles out-of-bounds conditions gracefully.

--- 2025/advent-07.fsx
+++ 2025/advent-07.fsx
@@ -39,16 +39,66 @@ module Example =
 |.|.|.|.|.|||.|"""
 
 
+
+module Array =
+    let get' i xs = Array.get xs i
+
 module Problem =
 
-    let parse (input: string) =
+    type Grid = (int * char) array array
+    
+    let parse (input: string) : Grid =
         input.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries)
         |> Array.map Seq.toArray
         |> Array.map Array.indexed
-        |> Array.indexed
+
+    let gridSize (grid: Grid) =
+        grid.Length, grid[0].Length
+            
+    let get2d r c (grid: Grid) =
+        grid[r] |> Array.get' c |> snd
+
+    let startPos (grid: Grid) =
+        0, grid[0] |> Array.find (fun (_, c) -> c = 'S') |> fst
+
+    let findSplitters (grid: Grid) =
+        grid
+        |> Array.mapi (fun r cells ->
+            cells
+            |> Array.filter (fun (_, ch) -> ch = '^')
+            |> Array.map (fun (c, _) -> (r, c)))
+        |> Array.collect id
+        |> Set.ofArray
+    
+    let solve1 (input: string) = 
+        let grid = parse input
+        let rows, cols = gridSize grid
+        let start = startPos grid
+        let splits = findSplitters grid
+        
+        let outOfBounds (r, c) =
+            r < 0 || r >= rows || c < 0 || c >= cols
+        
+        let rec loop pos (hits: Set<_>, visited: Set<_>) =
+            if (outOfBounds pos) || (visited |> Set.contains pos) then
+                hits, visited
+            else
+                let visited' = visited |> Set.add pos
+                if splits |> Set.contains pos then
+                    let hits' = hits |> Set.add pos
+                    let left = (fst pos, snd pos - 1)
+                    let right = (fst pos, snd pos + 1)
+                    loop right (loop left (hits', visited'))
+                else
+                    let down = (fst pos + 1, snd pos)
+                    loop down (hits, visited')
+        
+        loop start (Set.empty, Set.empty)
 
 
-// File.ReadAllText "2025/advent-07.txt"
-Example.given
-|> Problem.parse
-|> printfn "%A"
+File.ReadAllText "2025/input-07.txt"
+// Example.given
+|> Problem.solve1
+|> fst
+|> Set.count
+
